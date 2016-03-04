0ba4a9e76f11238ef7c7a5a03100a1c6ac82eebb
Brian Frickert <bfrickert@yahoo.com>
2016-03-04 14:06:30 -0500

    Added city name to map tab. Experimenting with green

--- shiny/helper.R
+++ shiny/helper.R
@@ -10,3 +10,17 @@ kpi.color <- function(x){
   else if (x < 201) return( 'orange')
   else return( 'red')
 }
+
+hate.kpi.color <- function(x){
+  col = 'red'
+  txt = 'were'
+  grp = 'groups'
+  if (x == 0) col = 'green'
+  else if (x==1) {
+    txt = 'was'
+    grp = 'group'
+  }
+  return(paste("there ", txt, " <strong><span style=\"color:", col, "\">",
+  x, " hate ", grp, "</span></strong> ", sep=''))
+  
+}
--- shiny/ui.R
+++ shiny/ui.R
@@ -3,10 +3,10 @@ source('helper.R')
 
 shinyUI(
   fixedPage(theme='bootstrap.css',
-            tags$h1("Awaygoing Data Analytics"),
+            tags$div(class="title", tags$h1("Awaygoing Data Analytics")),
             imageOutput('banner'),
                   br(),br(),br(),br(),br(),
-                  fixedRow(column(width=3,
+                  fixedRow(column(width=4,
                       selectInput('City','Potential Awaygoing City',
                                   with(arrange(joind.not.awaygoing, city), 
                                        paste(city, state, sep=', ')), selected='St. Louis, MO-IL'),
@@ -19,7 +19,7 @@ shinyUI(
                                    tags$h3('Similar Cities'),
                                    uiOutput('cluster.text'),
                                    uiOutput('cluster'),br(),br()), 
-                          tabPanel("US Map", uiOutput('map.text'), tags$hr(), imageOutput('map')),
+                          tabPanel("US Map", uiOutput("map.city.name"), uiOutput('map.text'), tags$hr(), imageOutput('map')),
                           tabPanel("Predicted Awaygoing Cities", HTML("<h2><strong>Awaygoing Cities of the Future</strong></h2>"), uiOutput("rf.text"), 
                                    tags$hr(), uiOutput("random.forest"), tags$br(), tags$br())
                         )
