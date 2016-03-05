fb245091da18d89ea0849461172e022f99e6a594
Brian Frickert <bfrickert@yahoo.com>
2016-03-05 15:04:57 -0500

    Tampa skyline now to the right of the display

--- shiny/ui.R
+++ shiny/ui.R
@@ -6,14 +6,11 @@ shinyUI(
             tags$h1(class="title","Awaygoing Data Analytics"),
             imageOutput('banner'),
                   br(),br(),br(),br(),br(),
-                  fixedRow(column(4,
-                                  tags$h3('Potential Awaygoing City'),
-                      selectInput('City', label=NULL,
-                                  with(arrange(joind.not.awaygoing, city), 
-                                       paste(city, state, sep=', ')), selected='St. Louis, MO-IL'),
-                      imageOutput('tampa')
-                  ),
-                    mainPanel(
+            tags$h3('Potential Awaygoing City'),
+            selectInput('City', label=NULL,
+                        with(arrange(joind.not.awaygoing, city), 
+                             paste(city, state, sep=', ')), selected='St. Louis, MO-IL'),
+            fixedRow(column(8,
                         tabsetPanel(
                           tabPanel("City Rankings", uiOutput("city.name"), uiOutput("rankings"), 
                                    tags$h3('Similar Cities'),
@@ -23,6 +20,8 @@ shinyUI(
                           tabPanel("Predicted Awaygoing Cities", HTML("<h2><strong>Awaygoing Cities of the Future</strong></h2>"), uiOutput("rf.text"), 
                                    tags$hr(), uiOutput("random.forest"), tags$br(), tags$br())
                         )
+                      ),column(4,br(),br(),
+                               imageOutput('tampa')
                       )
                     )
 ))
